1f84118409498fbfa3c942aa4a2d86a96d3c4d90
Nuri TIRAŞ <nuritiras@gmail.com>
2022-03-24 00:12:46 +0300

    Metin uzunluğu hesaplandı

---
+++ metin_uzunlugu.py
@@ -0,0 +1,23 @@
+# Misal 1
+
+kelime = 'Allah' # Uzunluğu 5
+print(len(kelime)) # len() fonksiyonu , bağımsız değişkenlerin içerdiği bir dizi karakter döndürür.
+
+
+# Misal 2
+
+bos = '' # Uzunluğu 0
+print(len(bos)) # Herhangi bir dize boş olabilir. 
+
+
+# Misal 3
+
+ben_kimim = 'ben\'kimim'    # Ters eğik çizgi (\) kaçış karakteri olarak kullanılan 
+print(len(ben_kimim))       # dizenin toplam uzunluğuna dahil değildir.
+
+''' 
+Çıktı:
+5
+0
+9
+'''
